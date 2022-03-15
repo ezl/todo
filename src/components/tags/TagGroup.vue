0999a10f0b5136d96ce8366fc57be3679842b99b
Yassin Youssoufm <yassinrsc@gmail.com>
2022-03-15 15:54:55 +0300

    remove unused code

--- src/components/tags/TagGroup.vue
+++ src/components/tags/TagGroup.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <div @mouseenter="onMouseEnter" @mouseleave="onMouseLeave" class="flex flex-wrap tags-wrapper">
+    <div class="flex flex-wrap tags-wrapper">
       <Tag v-for="tag in tags" :key="tag.id" :tag="tag" />
     </div>
   </div>
@@ -24,14 +24,6 @@ export default {
         .get();
     }
   },
-  methods: {
-    onMouseEnter() {
-      document.querySelector('.tag-settings-btn').classList.add('!visible');
-    },
-    onMouseLeave() {
-      document.querySelector('.tag-settings-btn').classList.remove('!visible');
-    }
-  }
 };
 </script>
 
