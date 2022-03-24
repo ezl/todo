4e84585b58b55fd4425d7c482c11e83af347f31a
Yassin Youssoufm <yassinrsc@gmail.com>
2022-03-24 18:15:34 +0300

    Remove “Inbox” from the navigation

--- src/newtab/App.vue
+++ src/newtab/App.vue
@@ -2,8 +2,6 @@
   <div class="h-full text-black dark:text-white md:py-3 md:px-8 lg:py-8 lg:px-20 text-base">
     <nav class="flex justify-between items-center my-2 mx-4">
       <ul class="flex ">
-        <li class="text-lg text-gray-400">Inbox</li>
-        <li class="separator bg-black dark:bg-white"></li>
         <li class="text-lg cursor-pointer">Tasks</li>
       </ul>
       <div>
@@ -25,11 +23,3 @@ export default {
   }
 };
 </script>
-
-<style scoped>
-.separator {
-  width: 1px;
-  height: 25px;
-  margin: 0 15px;
-}
-</style>
