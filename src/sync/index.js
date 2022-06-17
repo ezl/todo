9d94b92010837c63d0f24476a4293351e7fc2a38
Yassin Youssoufm <yassinrsc@gmail.com>
2022-06-17 12:54:33 +0300

    Fix an issue where syncing wouldn’t work if there was a network error while syncing

--- src/sync/index.js
+++ src/sync/index.js
@@ -3,28 +3,42 @@ import moment from 'moment';
 import axios from 'axios';
 import { handleServerChanges } from './server-change-handlers';
 
-let syncTimerId = null
+let syncTimerId = null;
+// The number of times we tried to sync with the server after the initial attempt failed
+let syncAttempts = 0;
+// After how many tries should we give up?
+let maxSyncAttempts = 3;
 
 // Schedules syncing, useful when we don’t want to sync immediately and delay it for few seconds
-export const syncDelayed = async () => {
-  // Avoid calling sync multiple times 
-  if(syncTimerId) return
+export const syncDelayed = async ms => {
+  // Avoid calling sync multiple times
+  if (syncTimerId) return;
 
-  syncTimerId = setTimeout( sync, 3000);
-}
+  if (ms === undefined) ms = 3000;
 
+  syncTimerId = setTimeout(sync, ms);
+};
 
 export const sync = async () => {
   const changeLogs = await LocalStorageHelper.getValue({ changeLogs: [] });
   const lastSyncedAt = await LocalStorageHelper.getValue({ lastSyncedAt: null });
 
   const latestLocalChangeLogs = changeLogs.filter(changeLog => changeLog.changed_at > lastSyncedAt);
-  
+
   try {
+    syncAttempts++;
+
     const response = await axios.post(`/sync/`, {
       last_synced_at: lastSyncedAt,
       change_logs: latestLocalChangeLogs
     });
+
+    if (response.status != 200) {
+      onSyncFailed();
+      return;
+    }
+
+    console.log('Synced!')
 
     if (response.data.last_synced_at) {
       LocalStorageHelper.setValue({ lastSyncedAt: response.data.last_synced_at });
@@ -34,13 +48,28 @@ export const sync = async () => {
 
     const latestRemoteChangeLogs = response.data.change_logs;
     handleServerChanges(latestRemoteChangeLogs);
-    clearTimeout(syncTimerId)
-    syncTimerId = null
+    resetDelayedSyncTimer()
+    syncAttempts = 0;
   } catch (error) {
     console.error(error);
+    onSyncFailed();
   }
 };
 
+const onSyncFailed = () => {
+  console.log(`Failed to synced. Attemps number: ${syncAttempts}`);
+  if (syncAttempts < maxSyncAttempts) {
+    console.log('retrying...');
+    resetDelayedSyncTimer()
+    syncDelayed(5000);
+  }else{
+    console.log(`Could not sync even after retrying ${syncAttempts} times`)
+    resetDelayedSyncTimer()
+    syncAttempts = 0
+  }
+};
 
-
-
+const resetDelayedSyncTimer = () => {
+  clearTimeout(syncTimerId);
+  syncTimerId = null;
+}
